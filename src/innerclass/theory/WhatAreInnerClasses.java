7625fca9194d5bdaf832c24e12577fb8456870b1
Samuel <samugit.projects@gmail.com>
2023-12-16 13:38:53 -0300

    Aula01 O que são inner classes

---
+++ src/innerclass/theory/WhatAreInnerClasses.java
@@ -0,0 +1,34 @@
+package innerclass.theory;
+
+@SuppressWarnings("ALL")
+public class WhatAreInnerClasses {
+
+    /*
+    *
+    * Representa o conceito de uma classe dentro de outra classe
+    *
+    * Existem vários tipos de inner classes com características específicas disponíveis no java
+    * Também são chamadas de Nested, ou seja, aninhadas, internas uma dentro da outra.
+    *
+    * Com relação a classificação temos:
+    *
+    *    1. Regular Inner Class (ou Nested Inner Class)
+    *    2. Static Nested Class (ou Static Inner Class)
+    *    3. Local Inner Class (ou Method Inner Class, ou ainda Method Local Inner Class)
+    *    4. Anonymous Inner Class (ela é importante porque é a base do funcionamento da programação funcional e expressões lambda no Java)
+    *
+    *      -----------------
+    *      |               |
+    *      |    CLASS      |
+    *      |               |
+     *     |               |
+     *     |   ---------   |
+     *     |   | CLASS |   |
+     *     |   |       |   |
+     *     |   |       |----------> Inner Class
+     *     |   |-------|   |
+     *     |               |
+     *     |---------------|
+     *
+    * */
+}
