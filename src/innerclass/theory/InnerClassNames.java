69d855ad540be1e9061203d879ba9ac1a7cd451e
Samuel <samugit.projects@gmail.com>
2023-12-18 16:38:28 -0300

    Aula06 Nomes das inner classes no processo compilação

---
+++ src/innerclass/theory/InnerClassNames.java
@@ -0,0 +1,20 @@
+package innerclass.theory;
+
+@SuppressWarnings("ALL")
+public class InnerClassNames {
+
+    /*
+    *
+    * Ao acessar a pasta out com arquivos byte code será possível observar como o Java trata as outer e inner classes
+    *
+    * Por exemplo:
+    *  A classe Car que possui a inner class Door poderá ser encontrada como:
+    *  Car$Door.class
+    *
+    *  Assim como a inner class Engine
+    *  Car$Engine.class
+    *
+    *  Na classe Operation podemos encontrar a inner class em:
+    *  Operation$Sum.class
+    * */
+}
