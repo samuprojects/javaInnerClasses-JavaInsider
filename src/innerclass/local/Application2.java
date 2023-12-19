295d4b181741b1c9c56dc20358b135d6158b5e4c
Samuel <samugit.projects@gmail.com>
2023-12-19 20:27:26 -0300

    Aula13 Como uma local inner class é compilada

--- src/innerclass/local/Application2.java
+++ src/innerclass/local/Application2.java
@@ -4,13 +4,14 @@ public class Application2 {
     private final String message = "Hi";
 
     public void greet(String name) {
-
-        int x = 10;
+        String upperName = name.toUpperCase();
+        // a visualização de variáveis que não foram alteradas ocorre porque se observar o arquivo compilado class essas inner classes são transformadas em
+        // top levels classes (que são as salvas em arquivo exclusivo .java) para uso pela JVM, se alterar essas variáveis há inconsistência, por isso essa obrigatoriedade.
 
         class Greeter {
             public void sayHi() {
 
-                System.out.format("%s, %s! %d", message, name, x); // as inner classes do tipo local podem acessar atributos, parametros ou outras variáveis locais da outer class
+                System.out.format("%s, %s!", message, upperName ); // as inner classes do tipo local podem acessar atributos, parametros ou outras variáveis locais da outer class
                 // as variáveis locais que podem ser acessadas nesse cenários devem ser explicitamente ou implicitamente final
                 // se alterar qualquer uma das variáveis que não explicitamente definidas como final o código deixará de compilar
             }
